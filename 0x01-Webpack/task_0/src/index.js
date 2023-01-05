00c7dedf047980723517034b86910b87b10b66a4
MounaBenAli <benalimouna1986@gmail.com>
2023-01-05 14:22:19 +0100

    0. Basic setup for Webpack static module bundler

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,5 @@
 import $ from "jquery";
 
-$("<p>Holberton Dashboard</p>").appendTo($('body'));
-$("<p>Dashboard data for the students</p>").appendTo($('body'));
-$("<p>Copyright - Holberton School</p>").appendTo($('body'));
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
