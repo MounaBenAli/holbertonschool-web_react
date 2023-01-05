7d2bc7bd4b67e4bbf34f060003667b353f1727f9
MounaBenAli <benalimouna1986@gmail.com>
2023-01-05 12:31:28 +0100

    0. Basic setup for Webpack static module bundler

---
+++ 0x01-Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$(<p>Holberton Dashboard</p>).appendTo($('body'));
+$(<p>Dashboard data for the students</p>).appendTo($('body'));
+$(<p>Copyright - Holberton School</p>).appendTo($('body'));
