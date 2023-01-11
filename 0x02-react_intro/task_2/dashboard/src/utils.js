b8a8a5dd7b684d9e707073d2109d01b8ba0a9bda
MounaBenAli <benalimouna1986@gmail.com>
2023-01-11 13:08:21 +0100

    2. Modify the App

---
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,11 @@
+export function getFullYear() {
+  return (
+   new Date().getFullYear()
+  )
+};
+
+export function getFooterCopy(isIndex) {
+    if (isIndex === true) return ('Holberton School')
+    return ('Holberton School main dashboard')
+};
+  
