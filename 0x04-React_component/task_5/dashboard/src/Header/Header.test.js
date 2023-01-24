f8292b111b34ae08bd32c1da2009227214286f0d
MounaBenAli <benalimouna1986@gmail.com>
2023-01-24 13:38:29 +0100

    9. Declare a pure component

---
+++ 0x04-React_component/task_5/dashboard/src/Header/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from './Header';
+
+
+// Tests for the Header component
+describe('Header component', () => {
+    it('should render without crashing', () => {
+      shallow(<Header />);
+    });
+  
+    it('should render an img and h1 tag', () => {
+      const wrapper = shallow(<Header />);
+      expect(wrapper.find('img').length).toBe(1);
+      expect(wrapper.find('h1').length).toBe(1);
+    });
+  });
