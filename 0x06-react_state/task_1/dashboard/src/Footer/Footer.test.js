2e47a38c2e5524387cbb949648f43d5c85ddca1d
MounaBenAli <benalimouna1986@gmail.com>
2023-02-07 19:27:03 +0100

    1. Controlled components and state callback functional login component

---
+++ 0x06-react_state/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+import adapter from '../../config/setupTests';
+
+// Tests for the Footer component
+describe('Footer component', () => {
+    it('should render without crashing', () => {
+      shallow(<Footer />);
+    });
+  
+    it('should render "Copyright" text', () => {
+        const wrapper = shallow(<Footer />);
+        expect(wrapper.text()).toContain('Copyright');
+    });
+  });
---
+++ 0x06-react_state/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+import Notifications from './Notifications';
+import adapter from '../../config/setupTests';
+
+
+describe('NotificationItem component', () => {
+  it('should render without crashing', () => {
+    shallow(<NotificationItem />);
+  });
+
+  it('should render correct type and value props', () => {
+    const wrapper = shallow(<NotificationItem type="default" value="test" />);
+    expect(wrapper.props().dataPriority === 'default');
+    expect(wrapper.props().dataValue === 'test');
+  });
+
+  it('Renders with props', () => {
+    const wrapper = shallow(
+      <NotificationItem type="urgent" html={{ __html: '<u>test</u>' }} />
+    );
+    expect(wrapper.props().dataPriority === 'urgent');
+    expect(wrapper.props().dataValue === 'test');
+    expect(wrapper.props().html === '<u>test</u>');
+  });
+});
+
+//testing the markAsRead property
+describe('testing the markAsRead property', () => {
+  let wrapper;
+  let mockMarkAsRead;
+  beforeEach(() => {
+    mockMarkAsRead = jest.fn();
+    wrapper = shallow(<NotificationItem id={1} markAsRead={mockMarkAsRead} />);
+  });
+
+  it('calls markAsRead with the correct id when clicked', () => {
+    wrapper.find('li').simulate('click');
+    expect(mockMarkAsRead).toHaveBeenCalledWith(1);
+  });
+});
