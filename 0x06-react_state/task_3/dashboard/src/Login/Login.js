7d4beef95abfd76eb6355f6ff49fc018beb18bc2
MounaBenAli <benalimouna1986@gmail.com>
2023-02-11 19:08:49 +0100

    3. Context consumer & advanced state

---
+++ 0x06-react_state/task_3/dashboard/src/Login/Login.js
@@ -0,0 +1,88 @@
+import React, { Fragment, useState } from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+
+const styles = StyleSheet.create({
+    login: {
+        margin: '40px',
+        font: '18px Helvetica, Arial, sans-serif',
+        fontSize: '15px',
+        marginRight: '9px',
+        marginLeft: '9px',
+    },
+    divSize: {
+        '@media (max-width: 900px)': {
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            margin: '5px 0',
+        }
+    },
+    inputSize: {
+        '@media (max-width: 900px)': {
+          width: '30%',
+          margin: '9px 0',
+      }
+    },
+});
+
+
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [enableSubmit, setEnableSubmit] = useState(false);
+
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        this.props.logIn(this.state.email, this.state.password);
+    };
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+        (email !== '' && password !== '') ? setEnableSubmit(true) : setEnableSubmit(false);
+    };
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+        (email !== '' && password !== '') ? setEnableSubmit(true) : setEnableSubmit(false);
+    };
+    
+    return (
+        <Fragment >
+            <div className={css(styles.divSize)}>
+                <p>Login to access the full dashboard</p>
+                <form onSubmit={handleLoginSubmit}>
+                    <label htmlFor="email"> Email:  </label>
+                    <input 
+                        type="text"
+                        id="email" 
+                        name="email" 
+                        placeholder="Please Enter Your Email" 
+                        className={css(styles.login, styles.inputSize)}
+                        value={email}
+                        onChange={handleChangeEmail}
+                    />
+                    <label htmlFor="password"> Password:  </label>
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        placeholder="Please Enter Your Password"
+                        className={css(styles.login, styles.inputSize)}
+                        value={password}
+                        onChange={handleChangePassword}
+                    />
+                    <input 
+                        type="submit"
+                        value="OK"
+                        className={css(styles.inputSize)}
+                        disabled={!enableSubmit}
+                    />
+                </form>
+            </div>
+        </Fragment>
+    );
+} 
+  
+export default Login;
