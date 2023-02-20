216f1d6048459f1dcde9689a16f74da7c2fbd6ca
MounaBenAli <benalimouna1986@gmail.com>
2023-02-20 11:23:58 +0100

    0. Converting into an Immutable object using fromJS

---
+++ 0x07-React_Immutable/0-fromjs.js
@@ -0,0 +1,14 @@
+import { fromJS } from 'immutable';
+
+export const getImmutableObject = (object) => fromJS(object);
+/* const obj = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132
+}
+const immutableMap = getImmutableObject(obj);
+
+
+console.log(immutableMap);
+ */
