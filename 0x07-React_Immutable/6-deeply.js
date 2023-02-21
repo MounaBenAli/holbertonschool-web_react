3b567aab138bcdb58aaf7c5d77349beafa069b26
MounaBenAli <benalimouna1986@gmail.com>
2023-02-21 11:09:56 +0100

    fix 6. Nested merge

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -1,32 +1,9 @@
-import Immutable from 'immutable';
+import { Map } from 'immutable';
 
+function mergeDeeplyElements(page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  return (map1.mergeDeep(map2));
+}
 
-export function mergeDeeplyElements(page1, page2) {
-    const map1 = Immutable.Map(page1);
-    const map2 = Immutable.Map(page2);
-    return (map1.mergeDeep(map2));
-};
-
-/* const page1 = {
-    'user-1': {
-      id: 1,
-      name: 'test',
-      likes: {
-        1: {
-          uid: 1234,
-        }
-      }
-    },
-};
-
-const page2 = {
-    'user-1': {
-      likes: {
-        2: {
-          uid: 134,
-        }
-      }
-    },
-};
-
-console.log(JSON.stringify(mergeDeeplyElements(page1, page2).toJS(), null, 2)); */
+export default mergeDeeplyElements;
