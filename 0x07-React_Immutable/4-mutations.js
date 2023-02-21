a0d41c6b43531b6554d8d35e9fe5593795223e17
MounaBenAli <benalimouna1986@gmail.com>
2023-02-21 10:38:17 +0100

    fix 4. Chained mutations

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,17 +1,14 @@
 import Immutable from 'immutable';
 
 export const map = Immutable.Map({
-    1: 'Liam',
-    2: 'Noah',
-    3: 'Elijah',
-    4: 'Oliver',
-    5: 'Jacob',
-    6: 'Lucas',
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
 });
 
-export const map2 = map
-.set('3', 'Benjamin')
-.set('5', 'Oliver');
-
-/* console.log(map.toJS());
-console.log(map2.toJS()); */
+export const map2 = map.withMutations((e) => {
+  e.set(2, 'Benjamin').set(4, 'Oliver');
+});
