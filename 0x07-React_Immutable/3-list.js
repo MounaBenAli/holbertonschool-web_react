7260502aa9eff3a16c0b2c53ebe6d082cb29a03e
MounaBenAli <benalimouna1986@gmail.com>
2023-02-21 10:25:32 +0100

    fix 3. List and push

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,11 +1,10 @@
 import Immutable from 'immutable';
 
 export function getListObject(array) {
-    return(Immutable.List(array));
-};
+  return (Immutable.List(array));
+}
 
 export function addElementToList(list, element) {
-    const immutableList = Immutable.List.isList(list) ? list : Immutable.list(list);
-    return (immutableList.push(list));
-};
-
+  const immutableList = Immutable.List.isList(list) ? list : Immutable.list(list);
+  return (immutableList.push(element));
+}
