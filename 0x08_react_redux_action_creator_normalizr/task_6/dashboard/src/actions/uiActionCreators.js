bf9a0dd78f66a1be841e1b42fd801c3e6b03c5ea
MounaBenAli <benalimouna1986@gmail.com>
2023-02-28 12:31:30 +0100

    6 fix Binding all UI actions

--- 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
+++ 0x08_react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -4,47 +4,59 @@ import {LOGIN,
         HIDE_NOTIFICATION_DRAWER } from "./uiActionTypes";
 import { bindActionCreators } from 'redux';
 
-//LOGIN
+// LOGIN
 export const login = (email, password) => {
     return {
         type: LOGIN,
         payload: { user : { email, password } }
     }
 };
-//bound the login action creator
+// Bound the login action creator
 export const boundLogin =  (email, password) => (dispatch) => {
     dispatch(login(email, password));
 };
 
-//LOGOUT
+// LOGOUT
 export const logout = () => {
     return {
         type: LOGOUT
     }
 };
-//bound the logout action creator
+// Bound the logout action creator
 export const boundLogout =  () => (dispatch) => {
     dispatch(logout());
 };
 
-//DISPLAY_NOTIFICATION_DRAWER
+// DISPLAY_NOTIFICATION_DRAWER
 export const displayNotificationDrawer = () => {
     return {
         type: DISPLAY_NOTIFICATION_DRAWER
     }
 };
-//bound the displayNotificationDrawer action creator
+// Bound the displayNotificationDrawer action creator
 export const boundDisplayNotificationDrawer =  () => (dispatch) => {
     dispatch(displayNotificationDrawer());
 };
 
-//HIDE_NOTIFICATION_DRAWER
+// HIDE_NOTIFICATION_DRAWER
 export const hideNotificationDrawer = () => {
     return {
         type: HIDE_NOTIFICATION_DRAWER
     }
 };
-//bound the hideNotificationDrawer action creator
+// Bound the hideNotificationDrawer action creator
 export const boundHideNotificationDrawer =  () => (dispatch) => {
     dispatch(hideNotificationDrawer());
 };
+
+// Binding all UI actions
+export const uiActions = {
+    login,
+    logout,
+    displayNotificationDrawer,
+    hideNotificationDrawer
+};
+
+export const boundUIActions = (dispatch) => bindActionCreators(uiActions, dispatch);
+
+
