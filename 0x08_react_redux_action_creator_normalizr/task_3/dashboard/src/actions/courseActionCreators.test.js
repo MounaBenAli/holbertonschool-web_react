c78879ec0a5d9ea7f8c69b2cb4c4ca4a3c6ce95e
MounaBenAli <benalimouna1986@gmail.com>
2023-02-28 09:56:41 +0100

    3. Create actions for the course list

---
+++ 0x08_react_redux_action_creator_normalizr/task_3/dashboard/src/actions/courseActionCreators.test.js
@@ -0,0 +1,21 @@
+import { selectCourse, unSelectCourse } from "./courseActionCreators";
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+
+//Tests for the action creators
+describe('Tests for the action creators', () => {
+    describe('test for the selectCourse action', () => {
+        it('should return: { type: SELECT_COURSE, index: 1 } when calling the creator with 1 as argument', () => {
+            const index = 1;
+            const expectedAction = { type: SELECT_COURSE, payload: index };
+            expect(selectCourse(index)).toEqual(expectedAction);
+        });
+    });
+    
+    describe('test for the unSelectCourse action', () => {
+        it('should return: { type: UNSELECT_COURSE, index: 1 } when calling the creator with 1 as argument', () => {
+            const index = 1;
+            const expectedAction = { type: UNSELECT_COURSE, payload: index };
+            expect(unSelectCourse(index)).toEqual(expectedAction);
+        });
+    });
+})
